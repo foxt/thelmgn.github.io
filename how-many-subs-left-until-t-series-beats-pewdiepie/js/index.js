6dfdd645b95470da78a700eb756a8dcfa0c52aa0
Leo Nesfield <contact@thelmgn.com>
2018-10-20 20:59:23 +0100

    animation tings

--- how-many-subs-left-until-t-series-beats-pewdiepie/js/index.js
+++ how-many-subs-left-until-t-series-beats-pewdiepie/js/index.js
@@ -1,12 +1,105 @@
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+
+;(function ( $, window, document, undefined ) {
+
+    var pluginName = "numerator",
+    defaults = {
+        easing: 'swing',
+        duration: 500,
+        delimiter: undefined,
+        rounding: 0,
+        toValue: undefined,
+        fromValue: undefined,
+        onStart: function(){},
+        onStep: function(){},
+        onProgress: function(){},
+        onComplete: function(){}
+    };
+
+    function Plugin ( element, options ) {
+        this.element = element;
+        this.settings = $.extend( {}, defaults, options );
+        this._defaults = defaults;
+        this._name = pluginName;
+        this.init();
+    }
+
+    Plugin.prototype = {
+        init: function () {
+            this.parseElement();
+            this.setValue();
+        },
+
+        parseElement: function () {
+            var elText = $(this.element).text().trim();
+
+            this.settings.fromValue = this.format(elText);
+        },
+
+        setValue: function() {
+            var self = this;
+
+            $({value: self.settings.fromValue}).animate({value: self.settings.toValue}, {
+
+                duration: parseInt(self.settings.duration),
+
+                easing: self.settings.easing,
+
+                start: self.settings.onStart,
+
+                step: function(now, fx) {
+                    
+                    $(self.element).text(numberWithCommas(self.format(now)));
+                    // accepts two params - (now, fx)
+                    self.settings.onStep(now, fx);
+                },
+
+                // accepts three params - (animation object, progress ratio, time remaining(ms))
+                progress: self.settings.onProgress,
+
+                complete: self.settings.onComplete
+            });
+        },
+
+        format: function(value){
+            if (this.settings.rounding < 1) {
+                if (typeof value == "string") {
+                  value = value.replace(/,/g,"")
+                }
+                return parseInt(value);
+            } else {
+                return parseFloat(value).toFixed(this.settings.rounding);
+            }
+        }
+    };
+
+    $.fn[ pluginName ] = function ( options ) {
+        return this.each(function() {
+            if ( $.data( this, "plugin_" + pluginName ) ) {
+                $.data(this, 'plugin_' + pluginName, null);
+            }
+            $.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
+
+        });
+    };
+
+})( jQuery, window, document );
+
+
+
+
+
+
 var pewSub = 0
 var oldPew = 0
 var tSub = 0
 var oldT = 0
 var gotPew = false
 var gotT = false
-function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
+
 String.prototype.toHHMMSS = function () {
   var delta = new Number(this)
     var days = Math.floor(delta / 86400);
@@ -66,44 +159,44 @@ setInterval(function() {
 var oldDiff = 0;
 
 function render() {
-  document.querySelector("#pewSub").innerText = numberWithCommas(pewSub)
-  document.querySelector("#tSub").innerText = numberWithCommas(tSub)
+  $("#pewSub").numerator({duration: "4900", rounding: "0", toValue: pewSub, easing: "linear"})
+  $("#tSub").numerator({duration: "4900", rounding: "0", toValue: tSub, easing: "linear"})
   if (oldT < tSub) {
-    document.querySelector("#tDiff").className = "plus"
-    document.querySelector("#tDiff").innerText = "+" + numberWithCommas(tSub - oldT)
+    document.querySelector("#tSub").className = "plus"
   } else if (tSub > tSub){
-    document.querySelector("#tDiff").className = "neg"
-    document.querySelector("#tDiff").innerText = numberWithCommas(tSub - oldT)
+    document.querySelector("#tSub").className = "neg"
   } else {
-    document.querySelector("#tDiff").className = "ind"
-    document.querySelector("#tDiff").innerText = "-0"
+    document.querySelector("#tSub").className = ""
   }
   if (oldPew < pewSub) {
-    document.querySelector("#pewDiff").className = "plus"
-    document.querySelector("#pewDiff").innerText = "+" + numberWithCommas(pewSub - oldPew)
+    document.querySelector("#pewSub").className = "plus"
   } else if (oldPew > pewSub) {
-    document.querySelector("#pewDiff").className = "neg"
-    document.querySelector("#pewDiff").innerText = numberWithCommas(pewSub - oldPew)
+    document.querySelector("#pewSub").className = "neg"
   } else {
-     document.querySelector("#pewDiff").className = "ind"
-    document.querySelector("#pewDiff").innerText = "+0"
+     document.querySelector("#pewSub").className = ""
   }
   oldT = tSub
   oldPew = pewSub
+  var diff = 0
   if (tSub > pewSub) {
     document.querySelector("#bg").className = "ts"
     document.querySelector("#winner").innerText = "T-Series"
-    document.querySelector("#subDiff").innerText = numberWithCommas(tSub - pewSub)
-    var result = (((tSub - pewSub) - Math.floor(oldDiff - (pewSub - tSub))) * 5).toString().toHHMMSS();
-    document.querySelector("#rate").innerText = result
-    oldDiff = tSub - pewSub
+    diff = tSub - pewSub
   } else {
     document.querySelector("#bg").className = "pew"
     document.querySelector("#winner").innerText = "Felix"
-    document.querySelector("#subDiff").innerText = numberWithCommas(pewSub - tSub)
-    var result = (((pewSub - tSub) - Math.floor(oldDiff - (pewSub - tSub))) * 5).toString().toHHMMSS();
-    document.querySelector("#rate").innerText = result
-      oldDiff = pewSub - tSub
+    diff = pewSub - tSub
     
   }
+  $("#subDiff").numerator({duration: "4900", rounding: "0", toValue: diff, easing: "linear"})
+    var result = ((diff - Math.floor(oldDiff - diff)) * 5).toString().toHHMMSS();
+    document.querySelector("#rate").innerText = result
+    if (oldDiff < diff) {
+      document.querySelector("#subDiff").className = "plus"
+    } else if (diff < oldDiff) {
+      document.querySelector("#subDiff").className = "neg"
+    } else {
+       document.querySelector("#subDiff").className = ""
+    }
+    oldDiff = diff
 }
