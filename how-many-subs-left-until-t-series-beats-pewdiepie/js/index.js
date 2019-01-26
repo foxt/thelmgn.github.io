ece458924c13e83ca5edcfd69dfdc43c9d668c60
Leo Nesfield <leo@thelmgn.com>
2019-01-26 17:41:51 +0000

    re-indent and opacity on load

--- how-many-subs-left-until-t-series-beats-pewdiepie/js/index.js
+++ how-many-subs-left-until-t-series-beats-pewdiepie/js/index.js
@@ -4,88 +4,88 @@ function numberWithCommas(x) {
 
 
 ;(function ( $, window, document, undefined ) {
-
-    var pluginName = "numerator",
-    defaults = {
-        easing: 'swing',
-        duration: 500,
-        delimiter: undefined,
-        rounding: 0,
-        toValue: undefined,
-        fromValue: undefined,
-        onStart: function(){},
-        onStep: function(){},
-        onProgress: function(){},
-        onComplete: function(){}
-    };
-
-    function Plugin ( element, options ) {
-        this.element = element;
-        this.settings = $.extend( {}, defaults, options );
-        this._defaults = defaults;
-        this._name = pluginName;
-        this.init();
-    }
-
-    Plugin.prototype = {
-        init: function () {
-            this.parseElement();
-            this.setValue();
+  
+  var pluginName = "numerator",
+  defaults = {
+    easing: 'swing',
+    duration: 500,
+    delimiter: undefined,
+    rounding: 0,
+    toValue: undefined,
+    fromValue: undefined,
+    onStart: function(){},
+    onStep: function(){},
+    onProgress: function(){},
+    onComplete: function(){}
+  };
+  
+  function Plugin ( element, options ) {
+    this.element = element;
+    this.settings = $.extend( {}, defaults, options );
+    this._defaults = defaults;
+    this._name = pluginName;
+    this.init();
+  }
+  
+  Plugin.prototype = {
+    init: function () {
+      this.parseElement();
+      this.setValue();
+    },
+    
+    parseElement: function () {
+      var elText = $(this.element).text().trim();
+      
+      this.settings.fromValue = this.format(elText);
+    },
+    
+    setValue: function() {
+      var self = this;
+      
+      $({value: self.settings.fromValue}).animate({value: self.settings.toValue}, {
+        
+        duration: parseInt(self.settings.duration),
+        
+        easing: self.settings.easing,
+        
+        start: self.settings.onStart,
+        
+        step: function(now, fx) {
+          
+          $(self.element).text(numberWithCommas(self.format(now)));
+          // accepts two params - (now, fx)
+          self.settings.onStep(now, fx);
         },
-
-        parseElement: function () {
-            var elText = $(this.element).text().trim();
-
-            this.settings.fromValue = this.format(elText);
-        },
-
-        setValue: function() {
-            var self = this;
-
-            $({value: self.settings.fromValue}).animate({value: self.settings.toValue}, {
-
-                duration: parseInt(self.settings.duration),
-
-                easing: self.settings.easing,
-
-                start: self.settings.onStart,
-
-                step: function(now, fx) {
-                    
-                    $(self.element).text(numberWithCommas(self.format(now)));
-                    // accepts two params - (now, fx)
-                    self.settings.onStep(now, fx);
-                },
-
-                // accepts three params - (animation object, progress ratio, time remaining(ms))
-                progress: self.settings.onProgress,
-
-                complete: self.settings.onComplete
-            });
-        },
-
-        format: function(value){
-            if (this.settings.rounding < 1) {
-                if (typeof value == "string") {
-                  value = value.replace(/,/g,"")
-                }
-                return parseInt(value);
-            } else {
-                return parseFloat(value).toFixed(this.settings.rounding);
-            }
+        
+        // accepts three params - (animation object, progress ratio, time remaining(ms))
+        progress: self.settings.onProgress,
+        
+        complete: self.settings.onComplete
+      });
+    },
+    
+    format: function(value){
+      if (this.settings.rounding < 1) {
+        if (typeof value == "string") {
+          value = value.replace(/,/g,"")
         }
-    };
-
-    $.fn[ pluginName ] = function ( options ) {
-        return this.each(function() {
-            if ( $.data( this, "plugin_" + pluginName ) ) {
-                $.data(this, 'plugin_' + pluginName, null);
-            }
-            $.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
-
-        });
-    };
-
+        return parseInt(value);
+      } else {
+        return parseFloat(value).toFixed(this.settings.rounding);
+      }
+    }
+  };
+  
+  $.fn[ pluginName ] = function ( options ) {
+    return this.each(function() {
+      if ( $.data( this, "plugin_" + pluginName ) ) {
+        $.data(this, 'plugin_' + pluginName, null);
+      }
+      $.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
+      
+    });
+  };
+  
 })( jQuery, window, document );
 
 
@@ -124,20 +124,20 @@ var gotT = false
 
 String.prototype.toHHMMSS = function () {
   var delta = new Number(this)
-    var days = Math.floor(delta / 86400);
-delta -= days * 86400;
-
-// calculate (and subtract) whole hours
-var hours = Math.floor(delta / 3600) % 24;
-delta -= hours * 3600;
-
-// calculate (and subtract) whole minutes
-var minutes = Math.floor(delta / 60) % 60;
-delta -= minutes * 60;
-
+  var days = Math.floor(delta / 86400);
+  delta -= days * 86400;
+  
+  // calculate (and subtract) whole hours
+  var hours = Math.floor(delta / 3600) % 24;
+  delta -= hours * 3600;
+  
+  // calculate (and subtract) whole minutes
+  var minutes = Math.floor(delta / 60) % 60;
+  delta -= minutes * 60;
+  
   return days + "d " + hours + "h " + minutes + "m"
 }
-    
+
 function checkChannel(channel,callback) {
   try {
     var xhttp = new XMLHttpRequest();
@@ -151,23 +151,28 @@ function checkChannel(channel,callback) {
   } catch(e) {}
 }
 function checkPew() {
+  document.querySelector("#pew").style.opacity = "0.6"
+  
   checkChannel(strings.pew.account,function(subs) {
-   pewSub = subs
+    pewSub = subs
     gotPew = true
     setTimeout(checkPew,5000)
- })
+    document.querySelector("#pew").style.opacity = "1"
+  })
 }
 function checkT() {
+  document.querySelector("#ts").style.opacity = "0.6"
   checkChannel(strings.ts.account,function(subs) {
     tSub = subs
     gotT = true
     setTimeout(checkT,5000)
- })
+    document.querySelector("#ts").style.opacity = "1"
+  })
 }
 
 function checkSub() {
- checkPew()
- checkT()
+  checkPew()
+  checkT()
 }
 checkSub()
 setInterval(function() {
@@ -197,7 +202,7 @@ function render() {
   } else if (oldPew > pewSub) {
     document.querySelector("#pewSub").className = "neg"
   } else {
-     document.querySelector("#pewSub").className = ""
+    document.querySelector("#pewSub").className = ""
   }
   oldT = tSub
   oldPew = pewSub
@@ -213,16 +218,16 @@ function render() {
     
   }
   $("#subDiff").numerator({duration: "4900", rounding: "0", toValue: diff, easing: "linear"})
-    var result = ((diff - Math.floor(oldDiff - diff)) * 5).toString().toHHMMSS();
-    document.querySelector("#rate").innerText = result
-    if (oldDiff < diff) {
-      document.querySelector("#subDiff").className = "plus"
-    } else if (diff < oldDiff) {
-      document.querySelector("#subDiff").className = "neg"
-    } else {
-       document.querySelector("#subDiff").className = ""
-    }
-    oldDiff = diff
+  var result = ((diff - Math.floor(oldDiff - diff)) * 5).toString().toHHMMSS();
+  document.querySelector("#rate").innerText = result
+  if (oldDiff < diff) {
+    document.querySelector("#subDiff").className = "plus"
+  } else if (diff < oldDiff) {
+    document.querySelector("#subDiff").className = "neg"
+  } else {
+    document.querySelector("#subDiff").className = ""
+  }
+  oldDiff = diff
 }
 
 function switchPew() {
