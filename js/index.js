5a619447532734992b4883b7acc42772378e0b74
Leo Nesfield <leo@thelmgn.com>
2019-04-13 15:59:36 +0100

    different animation

--- js/index.js
+++ js/index.js
@@ -1,7 +1,19 @@
+//https://pawelgrzybek.com/page-scroll-in-vanilla-javascript/
+function scrollIt(destination,duration=200,easing='linear',callback){const easings={linear(t){return t},easeInQuad(t){return t*t},easeOutQuad(t){return t*(2-t)},easeInOutQuad(t){return t<0.5?2*t*t:-1+(4-2*t)*t},easeInCubic(t){return t*t*t},easeOutCubic(t){return--t*t*t+1},easeInOutCubic(t){return t<0.5?4*t*t*t:(t-1)*(2*t-2)*(2*t-2)+1},easeInQuart(t){return t*t*t*t},easeOutQuart(t){return 1- --t*t*t*t},easeInOutQuart(t){return t<0.5?8*t*t*t*t:1-8* --t*t*t*t},easeInQuint(t){return t*t*t*t*t},easeOutQuint(t){return 1+ --t*t*t*t*t},easeInOutQuint(t){return t<0.5?16*t*t*t*t*t:1+16* --t*t*t*t*t}};const start=window.pageYOffset;const startTime='now'in window.performance?performance.now():new Date().getTime();const documentHeight=Math.max(document.body.scrollHeight,document.body.offsetHeight,document.documentElement.clientHeight,document.documentElement.scrollHeight,document.documentElement.offsetHeight);const windowHeight=window.innerHeight||document.documentElement.clientHeight||document.getElementsByTagName('body')[0].clientHeight;const destinationOffset=typeof destination==='number'?destination:destination.offsetTop;const destinationOffsetToScroll=Math.round(documentHeight-destinationOffset<windowHeight?documentHeight-windowHeight:destinationOffset);if('requestAnimationFrame'in window===false){window.scroll(0,destinationOffsetToScroll);if(callback){callback()}return}function scroll(){const now='now'in window.performance?performance.now():new Date().getTime();const time=Math.min(1,(now-startTime)/duration);const timeFunction=easings[easing](time);window.scroll(0,Math.ceil(timeFunction*(destinationOffsetToScroll-start)+start));if(window.pageYOffset===destinationOffsetToScroll){if(callback){callback()}return}requestAnimationFrame(scroll)}scroll()}
 function switchPane(from,to) {
-  document.querySelector(from).style.maxHeight = "0";
+  document.querySelector(from).style.transform = "translate(-100%,0%)";
+  window.scroll({
+    top: 0, 
+    left: 0, 
+    behavior: 'smooth',
+  });
+  scrollIt(0,500,'easeOutQuad');
   setTimeout(function() {
-    document.querySelector(to).style.maxHeight = "1000vh";
+    document.querySelector(from).style.display = "none"
+    document.querySelector(to).style.display = "block"
+    setTimeout(function() {
+      document.querySelector(to).style.transform = "translate(0%,0%)";
+    },15)
   },750)
   location.hash = to
 }
